1f593bde387e1ed053980062aeed86a577fa2c6a
suffynux <suffynux@gmail.com>
2025-06-16 12:42:57 +0500

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <section>
+        <nav className='bg-[#2B2B2B] flex justify-between align-center items-center  h-[40px] px-[20px] py-[4px]'>
+            <div className='text-[#FFFFFF] font-bold text-[20px] '>
+                logo
+            </div>
+            <div className='lg:hidden  '>
+                <img src="./icon _menu_.png" alt=""  className='   '/>
+            </div>
+        </nav>
+        {/* Hero */}
+        <div>
+
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default Navbar
